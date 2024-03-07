32c0ecac1bb4055592803b16f0be2c4fef681342
Wallison <wallisonzwka@gmail.com>
2024-03-07 13:56:20 -0300

    add_requirements

--- blog/views/post_view.py
+++ blog/views/post_view.py
@@ -6,7 +6,6 @@ class PostView(generic.ListView):
     queryset =  Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     
-    
 class PostDetail(generic.DetailView):
     model =  Post
     template_name = 'post_detail.html'
